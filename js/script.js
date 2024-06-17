0a39b6ad71704574685eaa220cac11093738f595
Pepijn <pepijnlatour@gmail.com>
2024-06-17 16:19:31 +0200

    added html files, about me and projects

--- js/script.js
+++ js/script.js
@@ -1,126 +1,124 @@
-const text = document.querySelector('.welcome_text');
-const content = document.querySelector('.content');
-const sectionimage = document.querySelector('.section-image');
 
-const welcomeText = [
-    '                  __                              __          __                            ^',
-    '                 / /___ __________  ___  _____   / /   ____ _/ /_____  __  _______          ^',
-    '         __  / / __ `/ ___/ __ \\/ _ \\/ ___/  / /   / __ `/ __/ __ \\/ / / / ___/       ^',
-    '           / /_/ / /_/ (__  ) /_/ /  __/ /     / /___/ /_/ / /_/ /_/ / /_/ / /              ^',
-    '    \\____/\\__,_/____/ .___/\\___/_/     /_____/\\__,_/\\__/\\____/\\__,_/_/        ^',
-    '                     /_/                                                          ^',
-];
-const iam = " I'm a software developer living ^ in the Netherlands";
+    const terminalcontainer = document.querySelector('.terminal-container');
+    const terminalUser = 'Visitor@website.JasperLatour;~$ ';
+    const terminalInput = document.getElementById('terminal-input');
+    const terminalOutput = document.getElementById('terminal-output');
 
-let i = 0;
-let j = 0;
-const speed = 5;
+    function terminalAnimate() {
+        terminalInput.focus();
+    }
 
-function welcomeAnimate(element, animatedText, speed, callback) {
-    if (i < animatedText.length) {
-        if (j < animatedText[i].length) {
-            if (animatedText[i].charAt(j) === "^") {
-                element.innerHTML += "<br>";
-            } else if (animatedText[i].charAt(j) === " ") { // Add non-breaking space
-                element.innerHTML += "&nbsp;";
+    function animateCommand(element, commandText, speed, callback) {
+        let i = 0;
+        function animate() {
+            if (i < commandText.length) {
+                if (commandText.slice(i, i+4) === '<br>') { // check for line breaks
+                    element.innerHTML += '<br>';
+                    i += 4;
+                } else if (commandText.charAt(i) === " ") { // check for tabs
+                    element.innerHTML += '&nbsp;';
+                    i++;
+                } else {
+                    element.innerHTML += commandText.charAt(i);
+                    i++;
+                }
+                setTimeout(animate, speed);
             } else {
-                element.innerHTML += animatedText[i].charAt(j);
+                // Animation complete, execute callback if provided
+                if (callback && typeof callback === 'function') {
+                    callback();
+                }
             }
-            j++;
-            setTimeout(() => welcomeAnimate(element, animatedText, speed, callback), speed);
-        } else {
-            i++;
-            j = 0;
-            setTimeout(() => welcomeAnimate(element, animatedText, speed, callback), speed);
         }
-    } else if (callback) {
-        callback();
+        animate();
     }
-}
 
-welcomeAnimate(text, welcomeText, 5, () => {
-    welcomeAnimate(content, iam, 5, () => {
-    content.style.display = 'block';
-    sectionimage.style.display = 'block';
-});});
+    function scrollToBottom() {
+        terminalInput.scrollIntoView({ behavior: 'smooth', block: 'end' });
+    }
 
+    // Variable to track if animation is in progress
+    let isAnimating = false;
 
-
-const terminalUser = 'Visitor@website.JasperLatour;~$ ';
-const terminal = document.getElementById('terminal');
-const terminalInput = document.getElementById('terminal-input');
-const terminalOutput = document.getElementById('terminal-output');
-
-function terminalAnimate() {
-    terminalInput.focus();
-}
-
-function animateCommand(element, commandText, speed) {
-    let i = 0;
-    function animate() {
-        if (i < commandText.length) {
-            if (commandText.slice(i, i+4) === '<br>') { // check for line breaks
-                element.innerHTML += '<br>';
-                i += 4;
-            } else if (commandText.charAt(i) === " ") { // check for tabs
-                element.innerHTML += '&nbsp;';
-                i++;
-            } else {
-                element.innerHTML += commandText.charAt(i);
-                i++;
-            }
-            setTimeout(animate, speed);
+    function command(input) {
+        // Check if animation is in progress
+        if (isAnimating) {
+            return; // Exit function if animation is ongoing
         }
+
+        // Set isAnimating to true to prevent overlapping animations
+        isAnimating = true;
+
+        switch (input) {
+            case 'clear':
+                terminalOutput.innerHTML = '';
+                isAnimating = false; // Reset animation flag
+                break;
+
+            case 'help':
+            case 'ls command':
+            case '?':
+            case 'h':
+            case 'Help':
+            case 'HELP':
+            case 'H':
+                terminalOutput.innerHTML += terminalInput.value + '<br>';
+                animateCommand(terminalOutput, 'Available commands: <br>' +
+                    'clear - Clear the terminal screen <br>' +
+                    'help - Display this help text<br>' +
+                    'projects - Display my projects<br>'
+                    , 50, function() {
+                        // Animation complete, allow next command
+                        isAnimating = false;
+                        scrollToBottom(); // Scroll to bottom after animation
+                    });
+                break;
+
+            case 'projects':
+            case 'Projects':
+            case 'project':
+            case 'Project':
+            case 'p':
+            case 'P':
+            case 'ls projects':
+                animateCommand(terminalOutput, 'My projects:<br>' +
+                    'Flipper - a home made dual flipper cast <br>' +
+                    'AutoMatic gasstation <br>' +
+                    'Self made audio mixer', 50, function() {
+                    isAnimating = false;
+                    scrollToBottom();
+                });
+                break;
+
+            case 'ls':
+                animateCommand(terminalOutput, 'About Me        Projects        Contact', 50, function() {
+                    isAnimating = false;
+                    scrollToBottom();
+                });
+                break;
+
+            default:
+                // Handle unknown commands or do nothing
+                isAnimating = false; // Ensure isAnimating is reset
+        }
+        scrollToBottom(); // Scroll to bottom after handling command
     }
-    animate();
-}
 
-function command(input) {
-    switch (input) {
-        case 'clear':
-            terminalOutput.innerHTML = '';
-            break;
-        case 'help':
-        case 'ls command':
-        case '?':
-        case 'h':
-        case 'Help':
-        case 'HELP':
-        case 'H':
-            terminalOutput.innerHTML += terminalInput.value + '<br>';
-            animateCommand(terminalOutput, 'Available commands: <br>' +
-                'clear - Clear the terminal screen <br>' +
-                'help - Display this help text<br>'      +
-                'projects - Display my projects<br>'
-            , 50, );
-            break;
+    terminalInput.addEventListener('keyup', function (e) {
+        if (e.key === 'Enter') {
+            terminalOutput.innerHTML += terminalUser + terminalInput.value + '<br>';
+            command(terminalInput.value);
+            terminalInput.value = '';
+        }
+    });
 
-        case 'projects':
-        case 'Projects':
-        case 'project':
-        case 'Project':
-        case 'p':
-        case 'P':
-        case 'ls projects':
-            animateCommand(terminalOutput, 'My projects:<br>' +
-                'Flipper - a home made dual filpper cast <br>' +
-                'AutoMatic gasstation <br>' +
-                'Self made audio mixer', 5);
-            break;
+    terminalInput.addEventListener('focus', function () {
+        terminalcontainer.classList.add('active');
+        console.log('Input focused');
+    });
 
-        case 'ls':
-            animateCommand(terminalOutput, 'About Me        Projects        Contact' , 50);
-    }
-}
-
-terminalInput.addEventListener('keyup', function (e) {
-    if (e.key === 'Enter') {
-
-        terminalOutput.innerHTML += terminalUser + terminalInput.value + '<br>';
-        command(terminalInput.value);
-        terminalInput.value = '';
-
-
-    }
-});
-terminalAnimate();
+    terminalInput.addEventListener('blur', function () {
+        terminalcontainer.classList.remove('active');
+        command('clear');
+        console.log('Input blurred');
+    });
---
+++ js/title.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const text = document.querySelector('.welcome_text');
+    const content = document.querySelector('.content');
+    const sectionimage = document.querySelector('.section-image');
+
+
+
+    const welcomeText = [
+        '                  __                              __          __                            ^',
+        '                 / /___ __________  ___  _____   / /   ____ _/ /_____  __  _______          ^',
+        '         __  / / __ `/ ___/ __ \\/ _ \\/ ___/  / /   / __ `/ __/ __ \\/ / / / ___/       ^',
+        '           / /_/ / /_/ (__  ) /_/ /  __/ /     / /___/ /_/ / /_/ /_/ / /_/ / /              ^',
+        '    \\____/\\__,_/____/ .___/\\___/_/     /_____/\\__,_/\\__/\\____/\\__,_/_/        ^',
+        '                     /_/                                                          ^',
+    ];
+
+    const iam = " I'm a software developer living ^ in the Netherlands";
+
+    let i = 0;
+    let j = 0;
+    const speed = 5;
+
+    function welcomeAnimate(element, animatedText, speed, callback) {
+        if (i < animatedText.length) {
+            if (j < animatedText[i].length) {
+                if (animatedText[i].charAt(j) === "^") {
+                    element.innerHTML += "<br>";
+                } else if (animatedText[i].charAt(j) === " ") { // Add non-breaking space
+                    element.innerHTML += "&nbsp;";
+                } else {
+                    element.innerHTML += animatedText[i].charAt(j);
+                }
+                j++;
+                setTimeout(() => welcomeAnimate(element, animatedText, speed, callback), speed);
+            } else {
+                i++;
+                j = 0;
+                setTimeout(() => welcomeAnimate(element, animatedText, speed, callback), speed);
+            }
+        } else if (callback) {
+            callback();
+        }
+    }
+
+    welcomeAnimate(text, welcomeText, 5, () => {
+        welcomeAnimate(content, iam, 5, () => {
+            content.style.display = 'block';
+            sectionimage.style.display = 'block';
+        });
+    });
+}, false);
