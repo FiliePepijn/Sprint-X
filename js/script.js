a06a6905526017a05d389a252831644b9da05227
Pepijn <pepijnlatour@gmail.com>
2024-06-19 02:32:15 +0200

    finished the website

--- js/script.js
+++ js/script.js
@@ -2,19 +2,18 @@ const terminalcontainer = document.querySelector('.terminal-container');
 const terminalUser = 'Visitor@website.JasperLatour;~$ ';
 const terminalInput = document.getElementById('terminal-input');
 const terminalOutput = document.getElementById('terminal-output');
+let discoInterval;
+let isAnimating = false;
 
-function terminalAnimate() {
-    terminalInput.focus();
-}
 
 function animateCommand(element, commandText, speed, callback) {
     let i = 0;
     function animate() {
         if (i < commandText.length) {
-            if (commandText.slice(i, i+4) === '<br>') { // check for line breaks
+            if (commandText.slice(i, i+4) === '<br>') {
                 element.innerHTML += '<br>';
                 i += 4;
-            } else if (commandText.charAt(i) === " ") { // check for tabs
+            } else if (commandText.charAt(i) === " ") {
                 element.innerHTML += '&nbsp;';
                 i++;
             } else {
@@ -36,28 +35,40 @@ function scrollToBottom() {
     terminalInput.scrollIntoView({ behavior: 'smooth', block: 'end' });
 }
 
-// Variable to track if animation is in progress
-let isAnimating = false;
+
+
+function startDisco() {
+    let hue = 0;
+    let page = document.querySelector('.star');
+    page.classList.add('disco');
+    let starElement = document.querySelector('.disco');
+    if (starElement) {
+        discoInterval = setInterval(() => {
+            starElement.style.filter = `hue-rotate(${hue}deg)`;
+            hue = (hue + 30) % 360;
+        }, 50);
+        localStorage.setItem('disco', 'on'); // Turn on disco
+    } else {
+        console.log('Star element not found');
+    }
+}
 
 
 
 function command(input) {
-    // Check if animation is in progress
     if (isAnimating) {
-        return; // Exit function if animation is ongoing
+        return;
     }
-
-    // Set isAnimating to true to prevent overlapping animations
     isAnimating = true;
 
-    let parts = input.split(' '); // Split the input into parts
-    let command = parts[0]; // The command is the first part
-    let argument = parts[1]; // The argument is the second part
+    let parts = input.split(' ');
+    let command = parts[0];
+    let argument = parts.slice(1).join(' ');
 
     switch (command) {
         case 'clear':
             terminalOutput.innerHTML = '';
-            isAnimating = false; // Reset animation flag
+            isAnimating = false;
             break;
 
         case 'help':
@@ -77,26 +88,22 @@ function command(input) {
                 'projects - Go to Projects page<br>' +
                 'cd - Change directory<br>'
                 , 50, function() {
-                    // Animation complete, allow next command
                     isAnimating = false;
-                    scrollToBottom(); // Scroll to bottom after animation
+                    scrollToBottom();
                 });
             break;
 
         case 'home':
-        case 'cd home':
         case 'cd /':
-            window.location.href = 'index.html';
+            window.location.href = '/Sprint-X/index.html';
                 break;
 
         case 'about':
-        case 'cd about':
         case 'cd home/about':
             window.location.href = '/Sprint-X/html/about.html';
             break;
 
         case 'projects':
-        case 'cd projects':
         case 'cd home/projects':
             window.location.href = '/Sprint-X/html/projects.html';
             break;
@@ -110,16 +117,44 @@ function command(input) {
                 'home/projects/pinball <br>' +
                 'home/projects/volumesliders <br>'
                 , 50, function() {
-                    // Animation complete, allow next command
+
                     isAnimating = false;
-                    scrollToBottom(); // Scroll to bottom after animation
+                    scrollToBottom();
                 });
             break;
+
+        case 'cd':
+            switch (argument) {
+                case 'home':
+                    window.location.href = 'index.html';
+                    break;
+                case 'about':
+                    window.location.href = '/Sprint-X/html/about.html';
+                    break;
+                case 'projects':
+                    window.location.href = '/Sprint-X/html/projects.html';
+                    break;
+                default:
+                    isAnimating = false;
+            }
+            break;
+
+        case 'disco':
+            console.log('Mario star command')
+            if (discoInterval) {
+                clearInterval(discoInterval);
+                localStorage.removeItem('disco');
+            } else {
+                startDisco();
+            }
+            isAnimating = false;
+            break;
+
         default:
-            // Handle unknown commands or do nothing
-            isAnimating = false; // Ensure isAnimating is reset
+
+            isAnimating = false;
     }
-    scrollToBottom(); // Scroll to bottom after handling command
+    scrollToBottom();
 }
 
 terminalInput.addEventListener('keyup', function (e) {
@@ -132,18 +167,20 @@ terminalInput.addEventListener('keyup', function (e) {
 
 terminalInput.addEventListener('focus', function () {
     terminalcontainer.classList.add('active');
-    console.log('Input focused');
 });
 
 terminalInput.addEventListener('blur', function () {
     terminalcontainer.classList.remove('active');
-    command('clear');
-    console.log('Input blurred');
 });
 
 terminalcontainer.addEventListener('click',
     function () {
         terminalInput.focus();
-        console.log('Container clicked');
     }
 );
+
+window.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('disco') === 'on') {
+        startDisco();
+    }
+});
